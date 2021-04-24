e0e6048d888aa9e9ef8259c09ae2bc6085861c92
Muhammad Usama <82697108+rana-sam@users.noreply.github.com>
2021-04-24 15:56:51 +0500

    oky

---
+++ delete.js
@@ -0,0 +1,12 @@
+let array=[12,23,6,44,96,09]
+let place=parseInt(prompt("Enter Index where you wants to delete an element"))
+switch (place) {
+    case place:
+        console.log("Before deletion "+array);
+        array.splice(place,1)
+        console.log("After deletion "+array);
+        break;
+
+    default:
+        break;
+}
